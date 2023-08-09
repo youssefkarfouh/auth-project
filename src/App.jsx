25b38eaca52b9eba174c136001e528876fedc68b
Youssef Karfouh <youssefkarfouh7@gmail.com>
2023-08-09 17:00:31 +0100

    changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Register from './Register'
+import Login from './Login'
+
+function App() {
+ 
+
+  return (
+
+  <Login/>
+    // <Register />
+  )
+}
+
+export default App
